c92091aba9f4c5c503d3beaae6813aa13f4e44c8
Ej Caparas <162857380+Artixtzy@users.noreply.github.com>
2026-03-30 23:50:09 +0800

    Add profile picture hover effects and toggle mode

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function toggleMode() {
     document.body.classList.toggle("light-mode");
 }
+
+const profilePic = document.getElementById("profilePic");
+
+profilePic.addEventListener("mouseover", function () {
+    profilePic.src = "hover.jpg";
+});
+
+profilePic.addEventListener("mouseout", function () {
+    profilePic.src = "ej.jpg";
+});
